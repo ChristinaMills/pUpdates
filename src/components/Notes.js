1ef5072e90ec675078dfbb43796018f2246c8842
Christina Mills <chrimills@gmail.com>
2018-06-12 11:06:26 -0700

    whaaaa

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -8,23 +8,7 @@ export default class Notes extends Component {
     super(props);
     this.state = {
       uid: '',
-      posts: [ 
-        // { postText: 'string of post text',
-        //   time: 'time string',
-        //   name: 'User name',
-        //   petName: 'name of pet'
-        // },
-        // { postText: '2string of post text',
-        //   time: '2time string',
-        //   name: '2User name',
-        //   petName: '2name of pet'
-        // },
-        // { postText: '3string of post text',
-        //   time: '3time string',
-        //   name: '3User name',
-        //   petName: '3name of pet'
-        // }
-      ]
+      posts: []
     };
   }
 
@@ -36,6 +20,7 @@ export default class Notes extends Component {
         });
         
         this.loadUserPostsFromFB();
+        // this.loadAllPostsFromFB();
       }
       else {
         console.log('NO USER');
@@ -58,53 +43,66 @@ export default class Notes extends Component {
       });
     });
   };
-
-    loadUserPostsFromFB = () => {
-      let collectionRef = db.collection('posts');
-      let query = collectionRef.where('userID', '==', this.state.uid);
-      // console.log('did you get here????   ');
-
-      query.get().then((querySnapshot) => {
-        querySnapshot.forEach((documentSnapshot) => {
-          // console.log(doc.id, '=>', doc.data());
-          let data = documentSnapshot.data();
-          console.log('this is the data     ', data);
-
-          this.setState({
-            posts: [
-              ...this.state.posts,
-              {
-                postContent: data.postContent,
-                time: data.time
-              }
-            ]
-            
-          });
-          console.log('*********** This is the STATE', this.state);
-          // console.log('****', doc.data.postContent);
-        });
-        // console.log(querySnapshot.docs);
-      });
-    };
- 
-
   
-    render(){
-      // const { notes, handleRemove, handleUpdate } = this.props;
+  loadUserPostsFromFB = async() => {
+    let postsRef = db.collection('posts');
+    let userRef = await postsRef.where('userID', '==', this.state.uid).get();
 
-
-      return (
-        <Fragment>
-          <h2>### Note-S component ###</h2>
-          <ul>{this.state.posts.map((post, index) => 
-            <li key={index}>{post.postContent}</li>)}
-          </ul>
-          <button onClick={this.loadUserPostsFromFB}>Press me to load</button>
-        
-        </Fragment>
-   
-      );
+    for(record of userRef.docs) {
+      console.log(record.id);
+      let newRef = await postsRef.doc(record.id).get();
+      console.log(newRef.data);
     }
+  };
+
+
+
+  // loadUserPostsFromFB = () => {
+  //   let collectionRef = db.collection('posts');
+  //   let query = collectionRef.where('userID', '==', this.state.uid);
+  //   // console.log('did you get here????   ');
+
+  //   query.select().get().then((querySnapshot) => {
+  //     querySnapshot.forEach((documentSnapshot) => {
+  //       // console.log(doc.id, '=>', doc.data());
+  //       let data = documentSnapshot.data();
+  //       console.log('this is the data     ', data);
+
+  //       this.setState({
+  //         posts: [
+  //           ...this.state.posts,
+  //           {
+  //             postContent: data.postContent,
+  //             time: data.time
+  //           }
+  //         ]
+            
+  //       });
+  //       console.log('*********** This is the STATE', this.state);
+  //       // console.log('****', doc.data.postContent);
+  //     });
+  //     // console.log(querySnapshot.docs);
+  //   });
+  // };
+ 
+   
+  
+  render(){
+    // const { notes, handleRemove, handleUpdate } = this.props;
+
+
+    return (
+      <Fragment>
+        <h2>### Note-S component ###</h2>
+        <ul>{this.state.posts.map((post, index) => 
+          <li key={index}>{post.postContent}</li>)}
+        </ul>
+        <button onClick={this.loadUserPostsFromFB}>Press me to load</button>
+        
+      </Fragment>
+   
+    );
+  }
 }
 
 // {/* <ul>{notes.map((note, index) => 
